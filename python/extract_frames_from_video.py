080cf97676e9ea3c9fe965e9cb4bd4628fea078c
Steffen Urban <steffen.urban@zeiss.com>
2022-06-22 08:59:31 +0200

    move function to utils

--- python/extract_frames_from_video.py
+++ python/extract_frames_from_video.py
@@ -1,35 +1,7 @@
 import cv2
 import os
 
-
-def extract_images_from_video_to_folder(video_file, img_savepath, resize_factor):
-
-    cap = cv2.VideoCapture(video_file)
-
-    if not os.path.exists(img_savepath):
-        os.makedirs(img_savepath)
-
-    invalid_images = 0
-    had_zero = False
-    print("Extracting frames")
-    while True:
-        ret, I = cap.read()
-        ts_ns = int(1e6*cap.get(cv2.CAP_PROP_POS_MSEC))
-        if not ret:
-            invalid_images += 1
-            if invalid_images > 100:
-                break
-            continue
-        if had_zero and ts_ns == 0:
-            continue
-
-        if not had_zero and ts_ns == 0:
-            had_zero = True
-        savepath = os.path.join(img_savepath, str(ts_ns)+".png")
-        I = cv2.resize(I, (0,0), fx=1./resize_factor, fy=1./resize_factor)
-        cv2.imwrite(savepath, I)
-        print("Extracted and saved to {}".format(savepath))
-
+from utils import extract_images_from_video_to_folder
 
 if __name__ == "__main__":
     import argparse
--- python/utils.py
+++ python/utils.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 import math
+import cv2
 
 ms_to_sec = 1./1000.
 s_to_nsec = 1e9
@@ -103,3 +104,35 @@ def time_to_s_nsec(timestamp_s):
     dec, sec = math.modf(timestamp_s) 
     nanosecs = dec * s_to_nsec
     return int(sec), int(nanosecs)
+
+
+
+
+def extract_images_from_video_to_folder(video_file, img_savepath, resize_factor):
+
+    cap = cv2.VideoCapture(video_file)
+
+    if not os.path.exists(img_savepath):
+        os.makedirs(img_savepath)
+
+    invalid_images = 0
+    had_zero = False
+    print("Extracting frames")
+    while True:
+        ret, I = cap.read()
+        ts_ns = int(1e6*cap.get(cv2.CAP_PROP_POS_MSEC))
+        if not ret:
+            invalid_images += 1
+            if invalid_images > 100:
+                break
+            continue
+        if had_zero and ts_ns == 0:
+            continue
+
+        if not had_zero and ts_ns == 0:
+            had_zero = True
+        savepath = os.path.join(img_savepath, str(ts_ns)+".png")
+        I = cv2.resize(I, (0,0), fx=1./resize_factor, fy=1./resize_factor)
+        cv2.imwrite(savepath, I)
+        print("Extracted and saved to {}".format(savepath))
+
